9454ee518b28aecedabc91365b8278e7a25d2fbf
Andy <amueller@ais.uni-bonn.de>
2013-01-22 17:09:54 +0100

    FIX redoing IgnoreVoidCRF: void label can be predicted. Otherwise we run into problems with enforcing the margin....

--- ignore_void_crf.py
+++ ignore_void_crf.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from pystruct.problems import GraphCRF
+from pystruct.inference import inference_dispatch
 
 
 from IPython.core.debugger import Tracer
@@ -13,79 +14,33 @@ class IgnoreVoidCRF(GraphCRF):
     """
     def __init__(self, n_states=2, n_features=None, inference_method='qpbo',
                  void_label=21):
+        if void_label >= n_states:
+            raise ValueError("void_label must be one of the states!")
         GraphCRF.__init__(self, n_states, n_features, inference_method)
         self.void_label = void_label
-
-    def psi(self, x, y):
-        """Feature vector associated with instance (x, y).
-
-        Feature representation psi, such that the energy of the configuration
-        (x, y) and a weight vector w is given by np.dot(w, psi(x, y)).
-
-        Parameters
-        ----------
-        x : tuple
-            Unary evidence.
-
-        y : ndarray or tuple
-            Either y is an integral ndarray, giving
-            a complete labeling for x.
-            Or it is the result of a linear programming relaxation. In this
-            case, ``y=(unary_marginals, pariwise_marginals)``.
-
-        Returns
-        -------
-        p : ndarray, shape (size_psi,)
-            Feature vector associated with state (x, y).
-
-        """
-        self._check_size_x(x)
-        features, edges = self.get_features(x), self.get_edges(x)
-        n_nodes = features.shape[0]
-
-        if isinstance(y, tuple):
-            # y is result of relaxation, tuple of unary and pairwise marginals
-            unary_marginals, pw = y
-            unary_marginals = unary_marginals.reshape(n_nodes, self.n_states)
-            # accumulate pairwise
-            pw = pw.reshape(-1, self.n_states, self.n_states).sum(axis=0)
-        else:
-            y = y.reshape(n_nodes)
-            gx = np.ogrid[:n_nodes]
-
-            #make one hot encoding
-            unary_marginals = np.zeros((n_nodes, self.n_states), dtype=np.int)
-            gx = np.ogrid[:n_nodes]
-            # nodes with void labels are "not assigned" - don't actually exist
-            mask = y != self.void_label
-            gx_masked = gx[mask]
-            y_masked = y[mask]
-            unary_marginals[gx_masked, y_masked] = 1
-
-            ##accumulated pairwise
-            pw = np.dot(unary_marginals[edges[:, 0]].T,
-                        unary_marginals[edges[:, 1]])
-
-        unaries_acc = np.dot(unary_marginals.T, features)
-        pw = pw + pw.T - np.diag(np.diag(pw))  # make symmetric
-
-        psi_vector = np.hstack([unaries_acc.ravel(),
-                                pw[np.tri(self.n_states, dtype=np.bool)]])
-        return psi_vector
 
     def loss(self, y, y_hat):
         # hamming loss:
         return np.sum((y != y_hat)[y != self.void_label])
 
+    def loss_augmented_inference(self, x, y, w, relaxed=False,
+                                 return_energy=False):
+        self.inference_calls += 1
+        self._check_size_w(w)
+        unary_potentials = self.get_unary_potentials(x, w)
+        pairwise_potentials = self.get_pairwise_potentials(x, w)
+        edges = self.get_edges(x)
+        # do loss-augmentation
+        for l in np.arange(self.n_states):
+            # for each class, decrement features
+            # for loss-agumention
+            unary_potentials[(y != l) * (y != self.void_label), l] += 1.
+        return inference_dispatch(unary_potentials, pairwise_potentials, edges,
+                                  self.inference_method, relaxed=relaxed,
+                                  return_energy=return_energy)
+
     def continuous_loss(self, y, y_hat):
         # continuous version of the loss
         # y is the result of linear programming
-        y_one_hot = np.zeros_like(y_hat)
         mask = y != self.void_label
-        gx = np.ogrid[:y.shape[0]]
-        gx_masked = gx[mask]
-        y_masked = y[mask]
-        y_one_hot[gx_masked, y_masked] = 1
-
-        # all entries minus correct ones
-        return np.prod(y.shape) - np.sum(y_one_hot * y_hat)
+        return GraphCRF.continuous_loss(self, y[mask], y_hat[mask])
--- tests/test_ignore_void_crf.py
+++ tests/test_ignore_void_crf.py
@@ -1,18 +1,26 @@
 import numpy as np
-from nose.tools import assert_equal
+
+from numpy.testing.utils import assert_array_equal
+from nose.tools import assert_almost_equal, assert_true
+
+from pystruct.learners import SubgradientStructuredSVM
+from pystruct.utils import find_constraint, exhaustive_inference
 
 from ..ignore_void_crf import IgnoreVoidCRF
 
+from IPython.core.debugger import Tracer
+tracer = Tracer()
+
 # make simple binary problem
-X = np.random.uniform(size=(20, 1, 2))
-Y = (X[:, :, 0] > .5).astype(np.int).reshape(20, -1)
+X = np.random.uniform(size=(20, 3, 2))
+Y = (X[:, :, 0] > .5).astype(np.int)
 # add void around decision boundary
 Y[np.abs(X[:, :, 0] - 0.5) < 0.1] = 2
 X = [(x, np.empty(shape=(0, 2), dtype=np.int)) for x in X]
 
 
-def test_energy():
-    crf = IgnoreVoidCRF(n_states=2, n_features=2, void_label=2,
+def test_inference():
+    crf = IgnoreVoidCRF(n_states=3, n_features=2, void_label=2,
                         inference_method='lp')
     # set some random weights, do inference and check that everything is ok
     np.random.seed(0)
@@ -21,16 +29,40 @@ def test_energy():
         y_hat, energy = crf.inference(x, w, relaxed=True,
                                       return_energy=True)
         energy_svm = np.dot(w, crf.psi(x, y_hat))
-        assert_equal(energy_svm, -energy)
+        assert_almost_equal(energy_svm, -energy)
+        y_hat_exhaustive = exhaustive_inference(crf, x, w)
+
+        y_hat = crf.inference(x, w)
+        assert_array_equal(y_hat, y_hat_exhaustive)
 
 
 def test_loss_augmented_inference():
-    crf = IgnoreVoidCRF(n_states=2, n_features=2, void_label=2,
+    crf = IgnoreVoidCRF(n_states=3, n_features=2, void_label=2,
                         inference_method='lp')
     np.random.seed(0)
     for x, y in zip(X, Y):
         w = np.random.normal(size=crf.size_psi)
         y_hat, energy = crf.loss_augmented_inference(x, y, w, relaxed=True,
                                                      return_energy=True)
-        assert_equal(np.dot(crf.psi(x, y_hat), w)
-                     + crf.continuous_loss(y, y_hat[0]), -energy)
+        assert_almost_equal(np.dot(crf.psi(x, y_hat), w) +
+                            crf.continuous_loss(y, y_hat[0]), -energy)
+
+
+def test_learning():
+    crf = IgnoreVoidCRF(n_states=3, n_features=2, void_label=2,
+                        inference_method='lp')
+    ssvm = SubgradientStructuredSVM(crf, verbose=10, C=100, n_jobs=1,
+                                    max_iter=50, learning_rate=0.01,
+                                    batch=False)
+    ssvm.fit(X, Y)
+
+    for x in X:
+        y_hat_exhaustive = exhaustive_inference(crf, x, ssvm.w)
+        y_hat = crf.inference(x, ssvm.w)
+        assert_array_equal(y_hat, y_hat_exhaustive)
+
+    constr = [find_constraint(crf, x, y, ssvm.w, y_hat=y_hat)
+              for x, y, y_hat in zip(X, Y, ssvm.predict(X))]
+    losses = [c[3] for c in constr]
+    slacks = [c[2] for c in constr]
+    assert_true(np.all(np.array(slacks) >= np.array(losses)))
