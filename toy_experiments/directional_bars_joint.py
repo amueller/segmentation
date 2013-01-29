371882e2f2ff32b7a0bfd6e1f166e2ea59335953
Andy <amueller@ais.uni-bonn.de>
2013-01-29 19:33:04 +0100

    fix directional bars example for new interface

--- toy_experiments/directional_bars_joint.py
+++ toy_experiments/directional_bars_joint.py
@@ -20,11 +20,12 @@ def main():
 
     # train a latent grid crf
     n_labels = len(np.unique(Y_train))
-    crf = LatentDirectionalGridCRF(n_labels=n_labels, n_states_per_label=6,
-                                   inference_method='ad3')
+    crf = LatentDirectionalGridCRF(n_labels=n_labels,
+                                   n_states_per_label=[1, 6],
+                                   inference_method='lp')
     clf = LatentSSVM(problem=crf, max_iter=50, C=10., verbose=2,
                      check_constraints=True, n_jobs=-1, break_on_bad=False,
-                     plot=True)
+                     tol=-10, base_svm='1-slack')
     clf.fit(X_train, Y_train)
 
     # the rest is plotting
@@ -34,13 +35,13 @@ def main():
         i = 0
         loss = 0
         for x, y, h_init, y_pred in zip(X_, Y_, H, Y_pred):
-            loss += np.sum(y != y_pred / crf.n_states_per_label)
+            loss += np.sum(y != y_pred)
             fig, ax = plt.subplots(3, 2)
-            ax[0, 0].matshow(y * crf.n_states_per_label,
-                             vmin=0, vmax=crf.n_states - 1)
+            ax[0, 0].matshow(y,
+                             vmin=0, vmax=crf.n_labels - 1)
             ax[0, 0].set_title("ground truth")
-            unary_pred = np.argmax(x, axis=-1) * crf.n_states_per_label
-            ax[0, 1].matshow(unary_pred, vmin=0, vmax=crf.n_states - 1)
+            unary_pred = np.argmax(x, axis=-1)
+            ax[0, 1].matshow(unary_pred, vmin=0, vmax=crf.n_labels - 1)
             ax[0, 1].set_title("unaries only")
             if h_init is None:
                 ax[1, 0].set_visible(False)
@@ -50,11 +51,10 @@ def main():
             ax[1, 1].matshow(crf.latent(x, y, clf.w),
                              vmin=0, vmax=crf.n_states - 1)
             ax[1, 1].set_title("latent final")
-            ax[2, 0].matshow(y_pred, vmin=0, vmax=crf.n_states - 1)
-            ax[2, 0].set_title("prediction")
-            ax[2, 1].matshow((y_pred // crf.n_states_per_label)
-                             * crf.n_states_per_label,
+            ax[2, 0].matshow(crf.inference(x, clf.w),
                              vmin=0, vmax=crf.n_states - 1)
+            ax[2, 0].set_title("prediction")
+            ax[2, 1].matshow(y_pred, vmin=0, vmax=crf.n_labels - 1)
             ax[2, 1].set_title("prediction")
             for a in ax.ravel():
                 a.set_xticks(())
