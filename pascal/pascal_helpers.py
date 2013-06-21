5f3d54bec4b59b2d687be933a042d7cbc8619879
Andy <amueller@ais.uni-bonn.de>
2013-06-21 13:41:14 +0200

    plot results for pascal

--- pascal/pascal_helpers.py
+++ pascal/pascal_helpers.py
@@ -1,9 +1,12 @@
 from collections import namedtuple
+import os
 
 import numpy as np
 from scipy.misc import imread
 from scipy import sparse
 from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
 
 from sklearn.externals.joblib import Memory
 from slic_python import slic_n
@@ -153,3 +156,48 @@ def get_kraehenbuehl_pot_sp(filename, superpixels):
     sp_probs = sp_probs.toarray()
     # renormalize (same as dividing by sp sizes)
     return sp_probs / sp_probs.sum(axis=-1)[:, np.newaxis]
+
+
+def plot_results(data, Y_pred, folder="figures", use_colors_predict=True):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    import matplotlib.colors as cl
+    np.random.seed(0)
+    random_colormap = cl.ListedColormap(np.random.uniform(size=(100, 3)))
+    for image_name, superpixels, y, y_pred in zip(data.file_names,
+                                                  data.superpixels, data.Y,
+                                                  Y_pred):
+        image = load_image(image_name)
+        fig, axes = plt.subplots(2, 3, figsize=(12, 6))
+        axes[0, 0].imshow(image)
+        axes[0, 1].set_title("ground truth")
+        axes[0, 1].imshow(image)
+        gt = get_ground_truth(image_name)
+        axes[0, 1].imshow(colors[gt], alpha=.7)
+        axes[1, 0].set_title("sp ground truth")
+        axes[1, 0].imshow(image)
+        axes[1, 0].imshow(colors[y[superpixels]], vmin=0, vmax=23, alpha=.7)
+
+        axes[1, 1].set_title("prediction")
+        axes[1, 1].imshow(image)
+        if use_colors_predict:
+            axes[1, 1].imshow(colors[y_pred[superpixels]], alpha=.7)
+        else:
+            vmax = np.max(np.hstack(Y_pred))
+            axes[1, 1].imshow(y_pred[superpixels], vmin=0, vmax=vmax, alpha=.9,
+                              cmap=random_colormap)
+        if use_colors_predict:
+            present_y = np.unique(np.hstack([y, y_pred]))
+        else:
+            present_y = np.unique(y)
+        present_y = present_y[present_y != 255]
+        axes[0, 2].imshow(colors[present_y, :][:, np.newaxis, :],
+                          interpolation='nearest')
+        for i, c in enumerate(present_y):
+            axes[0, 2].text(1, i, classes[c])
+        for ax in axes.ravel():
+            ax.set_xticks(())
+            ax.set_yticks(())
+        axes[1, 2].set_visible(False)
+        fig.savefig(folder + "/%s.png" % image_name, bbox_inches="tight")
+        plt.close(fig)
