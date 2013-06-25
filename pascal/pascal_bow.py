e73808e09084eac4db8604973382fe0bb9e4d49b
Andy <amueller@ais.uni-bonn.de>
2013-06-25 14:49:42 +0200

    pascal bow working

--- pascal/pascal_bow.py
+++ pascal/pascal_bow.py
@@ -8,8 +8,6 @@ from sklearn.svm import LinearSVC
 #from sklearn.metrics import recall_score, Scorer
 from slic_python import slic_n
 
-from pascal_helpers import load_pascal
-
 from latent_crf_experiments.utils import eval_on_sp
 from latent_crf_experiments.bow import SiftBOW
 
@@ -22,28 +20,29 @@ tracer = Tracer()
 def train_svm(C=0.1, grid=False):
     pascal = PascalSegmentation()
 
-    files_train = pascal.get_split("train1")
+    files_train = pascal.get_split("kTrain")
     superpixels = [slic_n(pascal.get_image(f), n_superpixels=100,
                           compactness=10)
                    for f in files_train]
-    bow = SiftBOW(pascal)
+    bow = SiftBOW(pascal, n_words=1000)
     data_train = bow.fit_transform(files_train, superpixels)
-    tracer()
 
     svm = LinearSVC(C=C, dual=False, class_weight='auto')
 
-    data_train = load_pascal("train1")
     X, y = np.vstack(data_train.X), np.hstack(data_train.Y)
     svm.fit(X, y)
     print(svm.score(X, y))
-    eval_on_sp(data_train, [svm.predict(x) for x in data_train.X],
+    eval_on_sp(pascal, data_train, [svm.predict(x) for x in data_train.X],
                print_results=True)
 
-    data_val = load_pascal("train2")
-    eval_on_sp(data_val, [svm.predict(x) for x in data_val.X],
+    files_val = pascal.get_split("kVal")
+    superpixels_val = [slic_n(pascal.get_image(f), n_superpixels=100,
+                              compactness=10) for f in files_val]
+    data_val = bow.transform(files_val, superpixels_val)
+    eval_on_sp(pascal, data_val, [svm.predict(x) for x in data_val.X],
                print_results=True)
 
     tracer()
 
 if __name__ == "__main__":
-    train_svm(C=0.001, grid=False)
+    train_svm(C=1, grid=False)
