de5d536258b9003fe4a752cf2ea619ea3e4f89b6
Andy <amueller@ais.uni-bonn.de>
2013-09-09 16:30:15 +0200

    add segment generation using itm, make stuff work on test-set.

--- nyu/nyu_helpers.py
+++ nyu/nyu_helpers.py
@@ -10,10 +10,14 @@ from datasets.nyu import NYUSegmentation
 
 from slic_python import slic_n
 from latent_crf_experiments.utils import (DataBunchNoSP, DataBunch, gt_in_sp,
-                                          probabilities_on_sp)
+                                          probabilities_on_sp, get_sp_normals)
 from latent_crf_experiments.pascal.pascal_helpers import merge_small_sp
+from latent_crf_experiments.hierarchical_segmentation import \
+    make_hierarchy_edges, HierarchicalDataBunch
 from skimage.segmentation import slic
 from skimage import morphology
+
+from information_theoretic_mst import ITM
 
 memory = Memory(cachedir="/home/data/amueller/cache")
 
@@ -28,7 +32,7 @@ def get_probabilities(file_name, path):
     return probabilities / 255.
 
 
-def load_single_file(dataset, file_name, n_sp=300, sp='rgb'):
+def load_single_file(dataset, file_name, n_sp=300, sp='rgb', reorder=None):
     print(file_name)
     image = dataset.get_image(file_name)
     if sp == 'rgb':
@@ -48,17 +52,23 @@ def load_single_file(dataset, file_name, n_sp=300, sp='rgb'):
 
     gt = gt_in_sp(dataset, file_name, sps)
     probs = get_probabilities(file_name, dataset.directory)
+    if reorder is not None:
+        probs = probs[:, :, reorder]
     probs_sp = probabilities_on_sp(dataset, probs, sps)
     return probs_sp, gt, sps
 
 
 @memory.cache
 def load_nyu(ds='train', n_sp=300, sp='rgb'):
-    # trigger cache 1.
+    # trigger cache.....
     dataset = NYUSegmentation()
     file_names = dataset.get_split(ds)
+    if ds == "test":
+        reorder = np.array([2, 0, 3, 1])
+    else:
+        reorder = None
     # load image to generate superpixels
-    result = Parallel(n_jobs=-1)(delayed(load_single_file)(dataset, f, n_sp, sp)
+    result = Parallel(n_jobs=-1)(delayed(load_single_file)(dataset, f, n_sp, sp, reorder=reorder)
                                  for f in file_names)
     X, Y, superpixels = zip(*result)
 
@@ -67,8 +77,12 @@ def load_nyu(ds='train', n_sp=300, sp='rgb'):
 
 @memory.cache
 def load_nyu_pixelwise(ds='train'):
+    if ds == "test":
+        reorder = np.array([2, 0, 3, 1])
+    else:
+        reorder = np.arange(4)
+    # trigger cache.
     dataset = NYUSegmentation()
-    # load image to generate superpixels
     file_names, X, Y = [], [], []
     for file_name in dataset.get_split(ds):
         print(file_name)
@@ -76,5 +90,35 @@ def load_nyu_pixelwise(ds='train'):
         gt = dataset.get_ground_truth(file_name)
         prediction = get_probabilities(file_name, dataset.directory)
         Y.append(gt)
-        X.append(prediction)
+        X.append(prediction[:, :, reorder])
     return DataBunchNoSP(X, Y, file_names)
+
+
+def compute_xyz_segments(dataset, data):
+    segments = []
+    for image_name, superpixels in zip(data.file_names, data.superpixels):
+        points_normals = dataset.get_pointcloud_normals(image_name)
+        centers3d = [np.bincount(superpixels.ravel(), weights=c.ravel())
+                   for c in points_normals[:, :, :3].reshape(-1, 3).T]
+        centers3d = (np.vstack(centers3d) / np.bincount(superpixels.ravel())).T
+        sp_normals = get_sp_normals(points_normals[:, :, 3:], superpixels)
+        km = ITM(n_clusters=30)
+        km.fit(np.hstack([centers3d, sp_normals]))
+        segments.append(km.labels_)
+    return segments
+
+def make_hierarchical_data(dataset, data):
+    segments = compute_xyz_segments(dataset, data)
+    hierarchy_edges = make_hierarchy_edges(segments, data.superpixels)
+    X_stacked = []
+    for x, y, edges in zip(data.X, data.Y, hierarchy_edges):
+        edges_stacked = np.vstack([x[1], edges])
+        # only add a single constant edge feature
+        hierarchy_edge_features = np.zeros((edges.shape[0], x[2].shape[1]))
+        hierarchy_edge_features[:, 0] = 1
+        edge_features_stacked = np.vstack([x[2], hierarchy_edge_features])
+        n_hidden = np.max(edges) + 1
+        x_stacked = (x[0], edges_stacked, edge_features_stacked, n_hidden)
+        X_stacked.append(x_stacked)
+    return HierarchicalDataBunch(X_stacked, data.Y, data.file_names,
+                                 data.superpixels, segments)
